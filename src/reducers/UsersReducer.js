4e4025d402c714d5eeb4e898e98974760ac2064e
nana <comfortappiah.ca@gmail.com>
2022-08-30 13:40:50 +0000

    redux setup

---
+++ src/reducers/UsersReducer.js
@@ -0,0 +1,24 @@
+let initialState = {
+	users: [
+		{
+			name: "John",
+			gen: 20,
+			email: "john@example.com",
+			id: "78478chukjchho3fw",
+		},
+		{
+			name: "Jane",
+			gen: 21,
+			email: "jane@gmail .com",
+			id: "hhheiuchieih939u494",
+		},
+	],
+};
+
+let UsersReducer = (state = initialState) => {
+	return state;
+};
+
+
+
+export default UsersReducer
---
+++ src/store/Store.js
@@ -0,0 +1,6 @@
+import { legacy_createStore as createStore } from "redux";
+import UsersReducer from "../reducers/UsersReducer";
+
+let Store = createStore(UsersReducer);
+
+export default Store;
